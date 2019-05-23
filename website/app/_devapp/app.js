4231e47123b1f890f6e6eac311454975d988958b
nwhahn <nathan@nwhahn.com>
2019-05-23 02:50:55 -0400

    Initial Commit 5-23

---
+++ website/app/_devapp/app.js
@@ -0,0 +1,32 @@
+import React, { Component, Fragment } from 'react';
+import { render } from 'react-dom';
+
+
+/** We are importing our index.php my app Vairaible */
+import myApp from 'myApp';
+
+/* globals __webpack_public_path__ */
+__webpack_public_path__ = `${window.STATIC_URL}/app/assets/bundle/`;
+
+class Myapp extends Component {
+    render() {
+
+        const { user : { name, email }, logged } = myApp;
+
+        return (
+            <Fragment>
+                <div className="dashboard">
+                    {logged &&
+                    <h2 className="status">Logged In</h2>
+                    }
+                    <h1 className="name"> {name}</h1>
+                    <p className="email">{email}</p>
+
+                    <p>API host variable {__API_HOST__}</p>
+                </div>
+            </Fragment>
+        )
+    }
+}
+
+render(<Myapp/>, document.getElementById('app'));
